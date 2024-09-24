fb59df1e5e98836b7523ad210480294541ea7e07
farukalways <alishopon367@gmail.com>
2024-09-24 10:00:22 +0600

    add Details of all your transaction in history button and local time add

--- js/donation.js
+++ js/donation.js
@@ -1,34 +1,6 @@
-// history button function
-  const historyTab = document.getElementById('history_btn');
-  const assetsBtn = document.getElementById('assets_btn');
-  const historyContant = document.getElementById('history_contant')
-
-  historyTab.addEventListener('click', function () {
-    historyTab.classList.add('bg-[#B4F461]');
-
-    assetsBtn.classList.remove('bg-[#B4F461]');
-
-    // main section hidden click in history button
-    
-    document.getElementById('main_container')
-    .classList.add('hidden');
-
-    document.getElementById('history_contant')
-    .classList.remove('hidden');
-
-  });
-
-  assetsBtn.addEventListener('click', function(){
-    assetsBtn.classList.add('bg-[#B4F461]');
-    historyTab.classList.remove('bg-[#B4F461]');
-   
-    document.getElementById('main_container')
-    .classList.remove('hidden');
-    document.getElementById('history_contant')
-    .classList.add('hidden');
-  })
 
 //Donate for Flood at Noakhali, Bangladesh section start
+
 document.getElementById('btn-donate-noakhali')
   .addEventListener('click', function () {
 
@@ -40,9 +12,22 @@ document.getElementById('btn-donate-noakhali')
       const newBalance = accountBlance - noakhaliDonate;
       document.getElementById('main_balance').innerText = newBalance;
       const donateNewBalance = noakhaliDonate + donateNoakhali;
-      document.getElementById('donate_noakhali').innerText = donateNewBalance
+      document.getElementById('donate_noakhali').innerText = donateNewBalance;
+
+      document.getElementById('input-donate-noakhali').value = '';
+
+      const historyContant = document.getElementById('history_contant');
+      const p = document.createElement('p');
+
+      const currentTime = new Date().toLocaleString(); 
+
+      // Add the donation amount and the time to the history
+      p.innerText = ` ${noakhaliDonate} Taka is Donated for Flood at Noakhali, Bangladesh; at ${currentTime}`;
+      historyContant.appendChild(p);
+
+
     } else {
-      alert('tui valo na')
+      alert('Invalid donate amount')
     }
   })
 
@@ -58,11 +43,24 @@ document.getElementById('btn_donate_feni')
       const newBalance = accountBlance - feniDonate
       document.getElementById('main_balance').innerText = newBalance
       const donateNewBalance = donatefeni + feniDonate;
-      document.getElementById('donate_feni').innerText = donateNewBalance
+      document.getElementById('donate_feni').innerText = donateNewBalance;
+
+      document.getElementById('input_donate_feni').value = '';
+
+      const historyContant = document.getElementById('history_contant');
+      const p = document.createElement('p');
+
+      const currentTime = new Date().toLocaleString(); 
+
+      // Add the donation amount and the time to the history
+      p.innerText = ` ${feniDonate} Taka is Donated for Flood at Feni, Bangladesh; at ${currentTime}`;
+      historyContant.appendChild(p);
+
     } else {
-      alert('hobana dada')
+      alert('Invalid donate amount')
     }
   })
+
 //Aid for Injured in the Quota Movement section start
 document.getElementById('btn_quota_movement')
   .addEventListener('click', function () {
@@ -73,12 +71,52 @@ document.getElementById('btn_quota_movement')
     if (0 < quotaMovement) {
       const newBalance = accountBlance - quotaMovement
       document.getElementById('main_balance').innerText = newBalance
-
       const donateQuota = donateQuotaBalance + quotaMovement;
       document.getElementById('quota_movement').innerText = donateQuota;
+      document.getElementById('input_quota_movement').value = '';
+      const historyContant = document.getElementById('history_contant');
+      const p = document.createElement('p')
+
+      const currentTime = new Date().toLocaleString(); 
+
+      // Add the donation amount and the time to the history
+      p.innerText = ` ${quotaMovement} Taka is Donated for Quota Movement, Bangladesh; at ${currentTime}`;
+      historyContant.appendChild(p);
 
     } else {
-      alert('hoba na dada')
+      alert('Invalid donate amount')
     }
 
   })
+
+
+// history button function
+const historyTab = document.getElementById('history_btn');
+const assetsBtn = document.getElementById('assets_btn');
+const historyContant = document.getElementById('history_contant')
+
+historyTab.addEventListener('click', function () {
+  historyTab.classList.add('bg-[#B4F461]');
+
+  assetsBtn.classList.remove('bg-[#B4F461]');
+
+  // main section hidden click in history button
+
+  document.getElementById('main_container')
+    .classList.add('hidden');
+
+  document.getElementById('history_contant')
+    .classList.remove('hidden');
+
+});
+
+assetsBtn.addEventListener('click', function () {
+  assetsBtn.classList.add('bg-[#B4F461]');
+  historyTab.classList.remove('bg-[#B4F461]');
+
+  document.getElementById('main_container')
+    .classList.remove('hidden');
+  document.getElementById('history_contant')
+    .classList.add('hidden');
+})
+
